73374f520f35d8ff7a634d82e323791eae0d0cd3
Hartmann Christophe <chartmann.35@gmail.com>
2020-01-01 16:05:30 +0100

    Add SonarQube status

---
+++ sonarqube-status.sh
@@ -0,0 +1,10 @@
+STATUS=$(curl -s "http://sonarqube.localhost:9000/api/qualitygates/project_status?projectKey=sas_judo" | jq -r .projectStatus.status)
+
+if [ $STATUS = "ERROR" ]
+then
+  echo "Qualitygate failed. ${STATUS}"
+  exit 1
+fi
+
+echo "Sonar Qualitygate is OK. ${STATUS}"
+exit 0
